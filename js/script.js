9c7d5af4d0cbb13c097539727738dcfda1ae1887
hamzagbouri <hamzagbouri2004@gmail.com>
2024-10-10 17:12:22 +0100

    push now

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navbar.classList.remove('open');
 };
+const counters = document.querySelectorAll('.number');
+const speed = 1000; // Change animation speed here
+
+counters.forEach(counter => {
+    const animate = () => {
+        const value = +counter.getAttribute('data-number');
+        const data = +counter.innerText;
+
+        const time = value / speed;
+        if(data < value) {
+            counter.innerText = Math.ceil(data + time);
+            setTimeout(animate, 10);
+        } else {
+            counter.innerText = value;
+        }
+    }
+    
+    animate();
+});
